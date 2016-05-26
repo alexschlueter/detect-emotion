efa15cf1d69a0432f24cfd0ef9300441303c7dc0
Johannes Stricker <johannesstricker@gmx.net>
2016-05-26 12:44:42 +0200

    Added KEY constants.

---
+++ header/controls.h
@@ -0,0 +1,13 @@
+#ifndef CONTROLS_H
+#define CONTROLS_H
+
+namespace controls {
+    const int KEY_SPACE = 32;
+    const int KEY_LEFT = 65361;
+    const int KEY_RIGHT = 65363;
+    const int KEY_0 = 48;
+    const int KEY_9 = 57;
+    const int KEY_N = 110;
+}
+
+#endif
