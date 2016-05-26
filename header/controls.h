40da71ffa3649b49b63a2e9dc440dbb76a8ad3c6
Johannes Stricker <johannesstricker@gmx.net>
2016-05-26 14:09:19 +0200

    Added Controls class to handle user input.

--- header/controls.h
+++ header/controls.h
@@ -1,13 +1,28 @@
 #ifndef CONTROLS_H
 #define CONTROLS_H
 
-namespace controls {
-    const int KEY_SPACE = 32;
-    const int KEY_LEFT = 65361;
-    const int KEY_RIGHT = 65363;
-    const int KEY_0 = 48;
-    const int KEY_9 = 57;
-    const int KEY_N = 110;
-}
+#include <map>
+#include <functional>
+
+class Controls
+{
+public:
+    enum {
+        KEY_SPACE = 32,
+        KEY_LEFT = 65361,
+        KEY_RIGHT = 65363,
+        KEY_0 = 48,
+        KEY_9 = 57,
+        KEY_N = 110
+    };
+
+    Controls(const std::map<int, std::function<void()>> &keyMap = std::map<int, std::function<void()>>());
+    bool HandleInput(int keyCode);
+    void Bind(int keyCode, std::function<void()> func);
+    bool Unbind(int keyCode);
+protected:
+private:
+    std::map<int, std::function<void()>> keyMap;
+};
 
 #endif
