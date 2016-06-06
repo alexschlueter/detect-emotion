23f5bb80b099badc735ca222b8562a974d104b8d
Alexander Schlüter <alx.schlueter@gmail.com>
2016-06-06 09:51:54 +0200

    pca doesn't work yet

--- src/playall.cpp
+++ src/playall.cpp
@@ -41,6 +41,49 @@ void selectPerson(int &selectedPerson, int select)
     selectedPerson = select;
 }
 
+cv::Mat normalize(const cv::Mat& src) {
+    cv::Mat srcnorm;
+    cv::normalize(src, srcnorm, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+    return srcnorm;
+}
+
+cv::PCA compressPCA(cv::InputArray pcaset, int maxComponents,
+                    const cv::Mat& testset, cv::OutputArray compressed)
+{
+    cv::PCA pca(pcaset, // pass the data
+                cv::Mat(), // there is no pre-computed mean vector,
+                   // so let the PCA engine to compute it
+            CV_PCA_DATA_AS_ROW, // indicate that the vectors
+                                // are stored as matrix rows
+                                // (use CV_PCA_DATA_AS_COL if the vectors are
+                                // the matrix columns)
+            maxComponents // specify how many principal components to retain
+            );
+    // if there is no test data, just return the computed basis, ready-to-use
+    cout << "ddfl"<<endl;
+    if( !testset.data )
+        return pca;
+    cout << "test"<<endl;
+    auto outmat = compressed.getMat();
+    // CV_Assert( testset.cols == pcaset.cols );
+
+    compressed.create(testset.rows, maxComponents, testset.type());
+
+    cv::Mat reconstructed;
+    for( int i = 0; i < testset.rows; i++ )
+    {
+        cv::Mat vec = testset.row(i), coeffs = outmat.row(i);
+        // compress the vector, the result will be stored
+        // in the i-th row of the output matrix
+        pca.project(vec, coeffs);
+        // and then reconstruct it
+        pca.backProject(coeffs, reconstructed);
+        // and measure the error
+        printf("%d. diff = %g\n", i, norm(vec, reconstructed, cv::NORM_L2));
+    }
+    return pca;
+}
+
 int main() {
     int font = cv::FONT_HERSHEY_SIMPLEX;
     string lmpath = "../../Landmark_Points_bin/landmarks0";
@@ -50,6 +93,8 @@ int main() {
 
     vector<VideoT> videos;
     vector<ActionUnit> aus;
+
+    cv::Mat featureMat;
 
     int maxFrames = 0;
     for (int k = 0; k < 10; ++k) {
@@ -65,6 +110,14 @@ int main() {
 
         string binaryfilename(lmpath + filestr + ".bin");
         videos.push_back(readBinaryFile(binaryfilename));
+        for (auto &frame : videos[k]) {
+            cv::Mat newrow;
+            for (auto &point : frame) {
+                newrow.push_back(point.x);
+                newrow.push_back(point.y);
+            }
+            featureMat.push_back(cv::Mat(newrow.t()));
+        }
 
         string actionfilename(aupath + filestr + "/SN0" + filestr + "_AUs.txt");
         auto actionunit = readActionUnitFromFile(actionfilename);
@@ -74,6 +127,10 @@ int main() {
     }
 
     cout << "loaded!" << endl;
+
+    cv::Mat(compressed);
+    auto pca = compressPCA(featureMat, CV_PCA_DATA_AS_ROW, 10, featureMat, compressed);
+    cout << "derp";
 
     int f = 0;
     int selected = -1;
