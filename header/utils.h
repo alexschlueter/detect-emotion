8d62d17647ee06aee82377ced5c3299f656254fa
Johannes Stricker <johannesstricker@gmx.net>
2016-06-07 15:46:18 +0200

    Added findMinInArray.

--- header/utils.h
+++ header/utils.h
@@ -16,7 +16,7 @@ cv::Point2f findMaxInArrayHelper(const cv::Point2f& a, const cv::Point2f & b){
 	return cv::Point2f(findMaxInArrayHelper(a.x,b.x),findMaxInArrayHelper(a.y,b.y));
 }
 
-template<typename T, int N> 
+template<typename T, int N>
 T findMaxInArray(const std::array<T,N> & array){
 	static_assert(N>0, "N has to be larger than zero");
 	T max = array[0];
@@ -26,4 +26,26 @@ T findMaxInArray(const std::array<T,N> & array){
 	return max;
 }
 
-#endif 
+template <typename T>
+T findMinInArrayHelper(const T & a, const T & b){
+    static_assert(std::is_arithmetic<T>::value, "Not Implemented findMinInArrayHelper");
+  return a < b ? a : b;
+}
+
+template <>
+cv::Point2f findMinInArrayHelper(const cv::Point2f& a, const cv::Point2f & b){
+    return cv::Point2f(findMinInArrayHelper(a.x,b.x),findMinInArrayHelper(a.y,b.y));
+}
+
+template<typename T, int N>
+T findMinInArray(const std::array<T,N> &array)
+{
+    static_assert(N>0, "N has to be larger than zero");
+    T min = array[0];
+    for(auto element : array)
+    {
+        min = findMinInArrayHelper(min, element);
+    }
+    return min;
+}
+#endif
