7d4ee9895bc68fa494355cd5a8212d2d68398f5f
Johannes Stricker <johannesstricker@gmx.net>
2016-06-14 09:54:27 +0200

    Added projection to PCA.

--- src/playone.cpp
+++ src/playone.cpp
@@ -35,27 +35,68 @@ void printMatrix(const cv::Mat &mat)
     std::cout << std::endl;
 }
 
-class PCA
+class PCAnalysis
 {
 public:
+    PCAnalysis()
+    {
+        // empty
+    }
+    PCAnalysis(const cv::Mat &data, unsigned int numComponents = 2, bool dataAsRow = true)
+    {
+        analyse(data, numComponents, dataAsRow);
+    }
+    PCAnalysis(const std::string &filepath)
+    {
+        fromFile(filepath);
+    }
     /**
      * Invokes a principal component analysis on data. As default, it expects
      * samples in rows and features in cols.
      */
     void analyse(const cv::Mat &data, unsigned int numComponents = 2, bool dataAsRow = true)
     {
-        _pca = cv::PCA(data, cv::Mat(), dataAsRow ? CV_PCA_DATA_AS_ROW : CV_PCA_DATA_AS_COL, numComponents);
-        _numComponents = numComponents;
+        _pca = cv::PCA(data, cv::Mat(), dataAsRow ? CV_PCA_DATA_AS_ROW : CV_PCA_DATA_AS_COL, (int)numComponents);
     }
-    /**
-     * Returns the number of principal components of the analysis.
-     */
-    int getNumComponents()
+
+    /** Projects data into the PCA space. */
+    void project(const cv::Mat &data, cv::Mat &output) const
     {
-        return _numComponents;
+        _pca.project(data, output);
     }
+
+    /** Projects data into the PCA space. */
+    cv::Mat project(const cv::Mat &data) const
+    {
+        return _pca.project(data);
+    }
+
+    /** Reconstructs original data from their PCA projection. */
+    void backProject(const cv::Mat &data, cv::Mat &output) const
+    {
+        _pca.backProject(data, output);
+    }
+
+    /** Reconstructs original data from their PCA projection. */
+    cv::Mat backProject(const cv::Mat &data) const
+    {
+        return _pca.backProject(data);
+    }
+
+    /** Returns the number of principal components of the analysis. */
+    int getNumComponents() const
+    {
+        return _pca.eigenvectors.rows;
+    }
+
+    /** Returns the dimension of the PCA. */
+    int getDimension() const
+    {
+        return _pca.eigenvectors.cols;
+    }
+
     /** Stores the results of the PCA to a file. */
-    void toFile(const std::string &filepath)
+    void toFile(const std::string &filepath) const
     {
         cv::FileStorage filestorage(filepath, cv::FileStorage::WRITE);
         filestorage << "mean" << _pca.mean;
@@ -63,6 +104,7 @@ public:
         filestorage << "eigenvalues" << _pca.eigenvalues;
         filestorage.release();
     }
+
     /** Tries to load the results of a PCA from a file. */
     bool fromFile(const std::string &filepath)
     {
@@ -85,8 +127,7 @@ public:
     }
 private:
     cv::PCA _pca;
-    int _numComponents;
-}
+};
 
 int main(int argc, char** argv) {
     if (argc < 2){
@@ -116,18 +157,16 @@ int main(int argc, char** argv) {
     /* Print */
     std::cout << landmarks.size() << " frames" << std::endl;
     std::cout << "Feature set is " << featureSet.rows << "x" << featureSet.cols << std::endl;
-    std::cout << "Loading PCA..." << std::endl;
-    cv::PCA pca;
-    if(!loadPCA("pca.xml", pca)) {
-        std::cout << "Starting PCA..." << std::endl;
-        pca = cv::PCA(featureSet, cv::Mat(), CV_PCA_DATA_AS_ROW, 6);
-        std::cout << "Finished PCA..." << std::endl;
-        savePCA("pca.xml", pca);
-    } else {
-        std::cout << "Loaded PCA!" << std::endl;
-    }
 
-    cv::Mat pointMat(1, 2*66, CV_32FC1);
+    std::cout << "Starting PCA..." << std::endl;
+    PCAnalysis pca(featureSet, 6, true);
+    std::cout << "Finished PCA. Dimension: " << pca.getDimension() << std::endl;
+    cv::Mat projected(featureSet.rows, featureSet.cols, CV_32FC1);
+    for(int i=0;i<projected.rows && featureSet.rows; i++)
+    {
+        projected.row(i) = pca.backProject(pca.project(featureSet.row(i)));
+        std::cout << "Error in row " << i << ": " << cv::norm(featureSet.row(i), projected.row(i)) << std::endl;
+    }
 
     cv::namedWindow("Image");
     for (auto frame : pointClouds) {
@@ -136,15 +175,6 @@ int main(int argc, char** argv) {
 
         img = cv::Mat::zeros(imageSize.y, imageSize.x, CV_8UC3);
 
-        // get matrix from points
-        for(int i = 0; i < 66; i++)
-        {
-            pointMat.at<float>(0, 2*i) = frame[i].x;
-            pointMat.at<float>(0, 2*i + 1) = frame[i].y;
-        }
-        cv::Mat projection;
-        pca.project(featureSet, projection);
-        std::cout << "Projection has size " << projection.rows << "x" << projection.cols << std::endl;
 
         for (int i = 0; i < 66; ++i) {
 
